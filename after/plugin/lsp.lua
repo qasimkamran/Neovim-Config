901c02249cc3d5135fe4f27a83429ccbf503e140
qasimk <qasimworkaus@gmail.com>
2025-07-15 11:24:53 +0000

    Updated version of ghosttest error info for new neovim version

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -108,3 +108,12 @@ cmp.setup({
     }),
 })
 
+vim.diagnostic.config({
+    virtual_text = {
+        prefix  = "●",
+        spacing = 2,
+    },
+    signs      = true,
+    underline  = true,
+    update_in_insert = false,
+})
